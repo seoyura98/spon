29b2921a3e02169b6d31be0eb8de0ec121315675
seoyura98 <dbfk333@gmail.com>
2025-02-25 17:13:11 +0900

    캠페인

--- js/캠페인.js
+++ js/캠페인.js
@@ -1,17 +1,31 @@
+/*header*/
+$(function () {
+  $(".bar").on("click", function () {
+    $(".wrap01").show();
+    $(this).hide();
+    $(".close").show();
+    $(".wrap02").show();
+  });
+  $(".close").on("click", function () {
+    $(".wrap01").hide();
+    $(this).hide();
+    $(".bar").show();
+    $(".wrap02").hide();
+  });
+});
+
+/*con4*/
 $(function () {
   $(".q01").on("click", function () {
     $(".a01").slideToggle();
-    $(".qna1").css({ color: "#222" });
     $(".qna1 span").css({ color: "#ff6f09" });
   });
   $(".q02").on("click", function () {
     $(".a02").slideToggle();
-    $(".qna2").css({ color: "#222" });
     $(".qna2 span").css({ color: "#ff6f09" });
   });
   $(".q03").on("click", function () {
     $(".a03").slideToggle();
-    $(".qna3").css({ color: "#222" });
     $(".qna3 span").css({ color: "#ff6f09" });
   });
 });
